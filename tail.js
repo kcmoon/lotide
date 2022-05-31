a53ed7f000e1aeeef807c63a174a956bcfdbb824
Kenny Moon <kennymoon90@gmail.com>
2022-05-30 21:13:12 -0700

    create a function that returns the tail of an array or an empty array if there isn't more than 1 element in the given array

---
+++ tail.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tailArr = [];
+  for (let i = 1; i < array.length; i++){
+    tailArr.push(array[i]);
+  }
+  return tailArr;
+};
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 1: Check the returned array elements
+const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result1.length, 2); // ensure we get back two elements
+assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: Check array with 1 element
+const result2 = tail(["Hello"]);
+assertEqual(result2.length, 0); // ensure we get back no elements
+assertEqual(result2, []); // ensure first element is empty array
+console.log(result2);
+
+// Test Case 3: Check array with no elements
+const result3 = tail([]);
+assertEqual(result3.length, 0); // ensure we get back no elements
+assertEqual(result3, []); // ensure first element is empty array
+console.log(result3);
