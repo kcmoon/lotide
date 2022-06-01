7fc132fd07b5265f8e00bdc522a4d6a836dacf49
Kenny Moon <kennymoon90@gmail.com>
2022-05-31 21:17:31 -0700

    fixed errors using eslint

--- eqArrays.js
+++ eqArrays.js
@@ -7,8 +7,8 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  arrStr1 = JSON.stringify(arr1);
-  arrStr2 = JSON.stringify(arr2);
+  let arrStr1 = JSON.stringify(arr1);
+  let arrStr2 = JSON.stringify(arr2);
 
   if (arrStr1 === arrStr2) {
     return true;
