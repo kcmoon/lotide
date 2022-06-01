0d2a2122cdbd8afc059dfa4edfd948edfb71d3cb
Kenny Moon <kennymoon90@gmail.com>
2022-05-31 20:52:40 -0700

    created a function to compare if 2 arrays are exactly the same

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  arrStr1 = JSON.stringify(arr1);
+  arrStr2 = JSON.stringify(arr2);
+
+  if (arrStr1 === arrStr2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
