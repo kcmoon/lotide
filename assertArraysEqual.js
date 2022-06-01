670afba4be39dec4ae58f0d0455ce64989663404
Kenny Moon <kennymoon90@gmail.com>
2022-05-31 22:27:18 -0700

    create an assertion function that uses the eqArrays function to compare 2 arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  let arrStr1 = JSON.stringify(arr1);
+  let arrStr2 = JSON.stringify(arr2);
+
+  if (arrStr1 === arrStr2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+};
