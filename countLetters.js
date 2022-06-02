bb69eaba7f26ba904c5c5dae9ce2528f5ebd460b
Kenny Moon <kennymoon90@gmail.com>
2022-06-01 19:05:33 -0700

    Created a function that takes in a string and outputs an object that counts the number of times each letter appears in the input string

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let results = {};
+  let newString = string.toLowerCase().replace(/ /g, "");
+  //console.log("newString", newString);
+  let letterKeys = "";
+
+  for (let letters of newString) {
+    //console.log("letters", letters)
+    if (results[letters]) {
+      results[letters] += 1;
+    } else {
+      results[letters] = 1;
+    }
+  }
+  //console.log("results", results);
+  return results;
+};
+
+
+let result1 = countLetters("Lighthouse in the House")
+
+assertEqual(result1.l, 1);
+assertEqual(result1.h, 4);
+assertEqual(result1.o, 2);
