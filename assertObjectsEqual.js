ee6928ea7ffdd914f30548744baa956ab51fcc66
Kenny Moon <kennymoon90@gmail.com>
2022-06-03 09:45:56 -0700

    Created a function to test functions that return objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let arrStr1 = JSON.stringify(arr1);
   let arrStr2 = JSON.stringify(arr2);
@@ -31,8 +23,8 @@ const eqObjects = function(object1, object2) {
   for (let key of obj1Keys) {
     //console.log("key", key)
     if (obj2Keys.includes(key)) {
-      //console.log("obj1Key", key);
-      //console.log('object1[key]', object1[key], 'object2[key]', object2[key] )
+      // console.log("obj1Key", key);
+      // console.log('object1[key]', object1[key], 'object2[key]', object2[key] )
       // if a and b both equal arrays => use eqArrays function
       if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
@@ -40,8 +32,8 @@ const eqObjects = function(object1, object2) {
         }
       } else {
         if (object1[key] !== object2[key]) {
-          //console.log("notMatch1", object1[key]);
-          //console.log('notMatch2', object2[key]);
+          // console.log("notMatch1", object1[key]);
+          // console.log('notMatch2', object2[key]);
           return false;
         }
       }
@@ -50,20 +42,17 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const abc = { a: "1", b: "2", c: "3" };
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4] };
+};
+let objA = { a: '1', b: 2 };
+let objB = { b: 2, a: '1' };
 
-//console.log(eqObjects(ab, abc));
-
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), false);
-
-
-
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), false);
+console.log(assertObjectsEqual(objA, objB));
