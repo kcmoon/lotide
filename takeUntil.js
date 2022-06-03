ff96b852142f390af66e739339f37c502576d857
Kenny Moon <kennymoon90@gmail.com>
2022-06-02 20:49:45 -0700

    Created a function that takes in an array and callback function that iterates over an array pushing each element to a new array until the callback function results to true

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  let arrStr1 = JSON.stringify(arr1);
+  let arrStr2 = JSON.stringify(arr2);
+
+  if (arrStr1 === arrStr2) {
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+ 
+
+// declare a function taking in an array and a callback function
+const takeUntil = function(array, callback) {
+  // push elements from given array to a new array to be returned
+  // stop pushing elements when the callback function retuns truthly
+  let result = [];
+
+  for (let element of array) {
+    if (!callback(element)) {
+      result.push(element);
+    } else {
+      return result;
+    }
+  }
+
+  return result;
+};
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+assertArraysEqual(takeUntil(data2, x => x.length > 5), ['I\'ve', 'been', 'to']);
