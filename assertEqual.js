8b89bf378bf4d29a3e063ea5755e07d701982dfe
Kenny Moon <kennymoon90@gmail.com>
2022-06-06 14:38:47 -0700

    refactored code in assertEqual.js and head.js as well as created test directory with test files for the two files

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(460, 460);
-assertEqual(460, 4600);
-assertEqual("Tool", "Tool");
-assertEqual("string", "String");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('./assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(460, 460);
+assertEqual(460, 4600);
+assertEqual("Tool", "Tool");
+assertEqual("string", "String");
