8b9182174a573ac6389063b44591d6dd0f741d39
Kenny Moon <kennymoon90@gmail.com>
2022-06-06 16:46:52 -0700

    cleaned up code by removing commented out tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([]), []);
+describe('#middle', () => {
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it('returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('returns [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
