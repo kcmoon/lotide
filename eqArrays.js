589120c1663e9a392e7f40f64bb2ee1265dae173
Kenny Moon <kennymoon90@gmail.com>
2022-06-06 15:19:19 -0700

    Refactored and exported function"

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let arrStr1 = JSON.stringify(arr1);
   let arrStr2 = JSON.stringify(arr2);
@@ -16,7 +8,4 @@ const eqArrays = function(arr1, arr2) {
   return false;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
